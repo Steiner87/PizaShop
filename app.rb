19ac32a73fcfdc485bfa6cefd57ef73acc4bfa53
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-08 21:34:37 +0200

    Add rakefile,update Gemfile and add Product model

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+set :database,"sqlite3:pizzashop.db"
+
+	class Product < ActiveRecord::Base
+		validates :name, presence: true, length: { minimum: 3}
+		validates :phone, presence: true, length: {is: 10}
+		validates :datestamp, presence: true
+	end
+
 
 get '/' do
 	erb :index
