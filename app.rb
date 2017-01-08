4f0609d1c406d752120a0ad2b4d4a6f94ba19bc2
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-08 21:51:55 +0200

    Dell some stings from app

--- app.rb
+++ app.rb
@@ -6,9 +6,7 @@ require 'sinatra/reloader'
 set :database,"sqlite3:pizzashop.db"
 
 	class Product < ActiveRecord::Base
-		validates :name, presence: true, length: { minimum: 3}
-		validates :phone, presence: true, length: {is: 10}
-		validates :datestamp, presence: true
+		
 	end
 
 
