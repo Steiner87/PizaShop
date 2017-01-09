04b742f23638c761dd2eba9fc29523b838035dcd
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-09 17:34:51 +0200

    Fill the produts table

---
+++ db/migrate/20170109124428_add_products.rb
@@ -0,0 +1,10 @@
+class AddProducts < ActiveRecord::Migration[5.0]
+  def change
+    Product.create  :title => 'Havaiian', :description =>'This is hawaian pizza', :price => 200, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_img => '/img/pizza01.png'
+
+    Product.create  :title => 'Kozazka', :description =>'This is Kozazkan pizza', :price => 210, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_img => '/img/pizza02.png'
+
+    Product.create  :title => 'Ohotnichja', :description =>'This is Ohotnichja pizza', :price => 220, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_img => '/img/pizza03.img'
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108201443) do
+ActiveRecord::Schema.define(version: 20170109124428) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
