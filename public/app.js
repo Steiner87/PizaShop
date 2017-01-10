dca86afeb45b221b2fd107b1f2145cb1a05ed029
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-10 09:49:16 +0200

    Update add_to_card function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
-function add_to_card(){
-    alert('Добавлено в корзину.');
+function add_to_card(id){
+var key='prodoct_'+id;
+    var x=window.localStorage.setItem(key);
+    x=x*1+1;
+    window.localStorage.getItem(key,x);
 }
 
 function foo(){
