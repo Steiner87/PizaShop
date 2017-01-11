b7b3c6a7acf5507f95fb0e8762de0edc2d0abea9
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-11 05:11:40 +0200

    Add cart_get_number_of_items() func

--- public/app.js
+++ public/app.js
@@ -1,10 +1,24 @@
-function add_to_card(id){
-var key='prodoct_'+id;
-    var x=window.localStorage.setItem(key);
-    x=x*1+1;
-    window.localStorage.getItem(key,x);
+function add_to_cart(id) {
+    var key = 'product_' + id;
+    var x = window.localStorage.getItem(key); //Первый раз x=NULL 
+    x = x * 1 + 1; //NULL*1=0 
+    window.localStorage.setItem(key, x);
+    alert('In your cart:' + cart_get_number_of_items(key));
 }
 
-function foo(){
+function foo() {
     alert('Hello ther!');
+}
+
+function cart_get_number_of_items() {
+    var cnt = 0;
+    for (var i = 0; i < window.localStorage.length; i++) {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+        if (key.indexOf('product_') == 0) {
+            cnt = cnt + value * 1;
+        }
+
+    }
+    return cnt;
 }
