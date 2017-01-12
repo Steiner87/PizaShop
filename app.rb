1a852465cacc6a3792674ea7458c034de5ea710f
Igor Pugachov <i.pugachev@makewear.com.ua>
2017-01-12 15:43:25 +0200

    Add cart page

--- app.rb
+++ app.rb
@@ -16,6 +16,32 @@ get '/' do
 	erb :index
 end
 
+# get '/cart' do
+# 	@products=Product.all
+# 	erb :cart
+# end
+
 post '/cart' do
-	erb "Hello!"
+	orders_input=params[:orders] 
+	@items=parse_order_input orders_input
+
+	@items.each do |item| 
+			item[0]=Product.find(item[0])
+	end
+	erb :cart
+end
+
+def parse_order_input orders_input
+	str1=orders_input.split(/,/)
+	arr=[]
+
+	str1.each do |x| 
+		str2=x.split(/\=/)
+		str3=str2[0].split(/_/)
+		id=str3[1];
+		cnt=str2[1]
+		arr2=[id,cnt]
+		arr.push arr2
+	end
+	return arr
 end
